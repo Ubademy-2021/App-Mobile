e01ff3faded8304ffbbd24d020fe2bdd6ec9ea5e
DamianGanopolsky <damian98dgg@gmail.com>
2021-09-26 15:25:19 -0300

    add specific file for handling the log in

--- App.js
+++ App.js
@@ -1,115 +1,17 @@
-import * as React from 'react';
-import { MaterialCommunityIcons, Ionicons } from '@expo/vector-icons';
+import LogInScreen from "./LogInScreen";
+
+
+import React from 'react'
+import {Text, View, StyleSheet, Image} from 'react-native'
+
 import logo from './assets/ubademy.png'
-import {View, StyleSheet, Image} from 'react-native'
-import {
-    NativeBaseProvider,
-    Box,
-    Text,
-    Heading,
-    VStack,
-    FormControl,
-    Input,
-    Link,
-    Button,
-    Icon,
-    IconButton,
-    HStack,
-    Divider,
-} from 'native-base';
 
-
-export default function App() {
-    const [email, setEmail] = React.useState(); /*En email se guardara el email que se ha escrito */
-    const [password, setPassword] = React.useState();
+const App =() =>{
     return (
-        <NativeBaseProvider>
-            <Box safeArea flex={1} p="2" py="8" w="90%" mx="auto">
-                <View style={{justifyContent: 'center',alignItems: 'center'}}>
-                    <Image
-                        source={logo}
-                        style={{height: 100, width:156 }}
-                    />
-
-                    <Heading size="lg" fontWeight="600" color="coolGray.800">
-                        Welcome to Ubademy!
-                    </Heading>
-                    <Heading mt="1" color="coolGray.600" fontWeight="medium" size="xs">
-                        Sign in to continue
-                    </Heading>
-                </View>
-                <VStack space={3} mt="5">
-                    <FormControl>
-                        <FormControl.Label
-                            _text={{
-                                color: 'coolGray.800',
-                                fontSize: 'xs',
-                                fontWeight: 500,
-                            }}>
-                            Email ID
-                        </FormControl.Label>
-                        <Input
-                            value={email}
-                            onChangeText={(anything) => setEmail(anything)}
-                        />
-                    </FormControl>
-                    <FormControl>
-                        <FormControl.Label
-                            _text={{
-                                color: 'coolGray.800',
-                                fontSize: 'xs',
-                                fontWeight: 500,
-                            }}>
-                            Password
-                        </FormControl.Label>
-                        <Input
-                            type="password"
-                            value={password}
-                            onChangeText={(anything) => setPassword(anything)}
-                        />
-                    </FormControl>
-                    <Button
-                        mt="2"
-                        colorScheme="indigo"
-                        _text={{ color: 'white' }}
-                        onPress={() =>{
-                            if(email){
-                                console.log("Navego a la siguiente pantalla");
-                            }
-                            else{
-                                window.alert("Invalid log in. Please provide a valid username");
-                            }
-                        }
-                        }
-                    >
-                        Sign in
-                    </Button>
-                    <Button
-                        mt="2"
-                        colorScheme="blue"
-                        _text={{ color: 'white' }}
-                        onPress={() => window.alert("Not implemented yet")}
-                    >
-                        Sign in with Facebook
-                    </Button>
-                    <HStack mt="6" justifyContent="center">
-                        <Text fontSize="sm" color="muted.700" fontWeight={400}>
-                            I'm a new user.{' '}
-                        </Text>
-                        <Link
-                            _text={{
-                                color: 'indigo.500',
-                                fontWeight: 'medium',
-                                fontSize: 'sm',
-                            }}
-                            href="#">
-                            Sign Up
-                        </Link>
-                    </HStack>
-                </VStack>
-            </Box>
-        </NativeBaseProvider>
+        LogInScreen()
     );
-}
+};
+
+export default App;
 
 
