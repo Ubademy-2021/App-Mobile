a8486971d17f14e090b9b32251342db4ab50a756
DamianGanopolsky <damian98dgg@gmail.com>
2021-10-20 15:33:27 -0300

    basic screen template

---
+++ screens/RegisterOptionsScreen.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { View, StyleSheet, Image } from 'react-native'
+
+import {
+  NativeBaseProvider,
+  Box,
+  Text,
+  Heading,
+  VStack,
+  FormControl,
+  Input,
+  Link,
+  Button,
+  IconButton,
+  HStack,
+  Divider
+} from 'native-base'
+
+export default function RegisterOptionsScreen ({ navigation }) {
+  return (
+        <Text>hola </Text>
+  )
+}
