78b3d087f2666e4199aa208f01f2e85d61a59483
DamianGanopolsky <damian98dgg@gmail.com>
2021-10-20 15:54:35 -0300

    change height of buttons

--- screens/SignUpOptionsScreen.js
+++ screens/SignUpOptionsScreen.js
@@ -20,8 +20,28 @@ export default function SignUpOptionsScreen ({ navigation }) {
   return (
       <NativeBaseProvider>
         <Box safeArea flex={1} p="2" py="8" w="90%" mx="auto">
-          <Text> Sign up with facebook </Text>
-
+          <Text numberOfLines={1}></Text>
+          <Text numberOfLines={1}></Text>
+          <Text numberOfLines={1}></Text>
+          <Text numberOfLines={1}></Text>
+          <Button
+              mt="2"
+              colorScheme="indigo"
+              onPress={() => {
+                navigation.navigate('Signup')
+              }
+              }
+          >
+            Sign up with e-mail
+          </Button>
+          <Text numberOfLines={9}></Text>
+          <Button
+              mt="2"
+              colorScheme="blue"
+              onPress={() => window.alert('Not implemented yet')}
+          >
+            Sign up with Facebook
+          </Button>
         </Box>
       </NativeBaseProvider>
   )
