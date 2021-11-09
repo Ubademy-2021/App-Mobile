de8a1cd068a123ad4b1bcd2317800e0963178552
Franco <alasinfranco3@gmail.com>
2021-11-09 00:25:54 -0300

    Select dropdowns added

--- App.js
+++ App.js
@@ -8,19 +8,21 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import SignUpOptionsScreen from './screens/SignUpOptionsScreen'
+import StudentCourseSearchScreen from './screens/StudentCourseSearchScreen'
 
 const Stack = createNativeStackNavigator()
 
 const App = () => {
   return (
    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
+      <Stack.Navigator initialRouteName="StudentCourseSearch">
           <Stack.Screen name="Login" component={LogInScreen} options={{ title: 'Welcome' }}/>
           <Stack.Screen name="SignupOptions" component={SignUpOptionsScreen}/>
           <Stack.Screen name="Signup" component={SignUpScreen} />
           <Stack.Screen name="Interests" component={InterestsScreen} options={{ title: 'Interests' }}/>
           <Stack.Screen name="Location" component={LocationScreen} />
           <Stack.Screen name="ProfileSelection" component={ProfileSelectionScreen} />
+          <Stack.Screen name="StudentCourseSearch" component={StudentCourseSearchScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   )
