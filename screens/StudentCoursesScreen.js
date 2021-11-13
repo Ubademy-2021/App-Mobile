5e4e827800245a8221923a775e87c2f155484b1f
Franco <alasinfranco3@gmail.com>
2021-11-12 23:02:12 -0300

    dummy files added to show footbar

---
+++ screens/StudentCoursesScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const StudentCoursesScreen = () => {
+  return (
+    <View>
+      <Text>My COurses</Text>
+    </View>
+  )
+}
+export default StudentCoursesScreen
---
+++ screens/SusbscriptionScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const SubscriptionScreen = () => {
+  return (
+    <View>
+      <Text>Subs</Text>
+    </View>
+  )
+}
+export default SubscriptionScreen
