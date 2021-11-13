26079da10b9135fdeff8aa54b7c958a9b147e8a7
DamianGanopolsky <damian98dgg@gmail.com>
2021-11-13 17:36:00 -0300

    add subscription button

--- screens/SusbscriptionScreen.js
+++ screens/SusbscriptionScreen.js
@@ -1,11 +1,51 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { ImageBackground, View, StyleSheet, Platform } from 'react-native'
+
+import {
+    Avatar,
+    Box,
+    Center,
+    Text,
+    VStack,
+    IconButton,
+    HStack,
+    NativeBaseProvider,
+    ScrollView,
+    Button
+} from 'native-base'
+
+const apiGatewayBaseUrl = 'https://ubademy-api-gateway.herokuapp.com/api-gateway/'
 
 const SubscriptionScreen = () => {
+
+
+    const getSuscriptionsURL = apiGatewayBaseUrl + 'suscriptions'
+
+    const getStudentCourses = () => {
+        return fetch(getSuscriptionsURL)
+            .then((response) => response.json())
+            .then((json) => {
+                console.log(json);
+            })
+            .catch((error) => {
+                console.error(error)
+            })
+    }
+
   return (
-    <View>
-      <Text>Not implemented yet</Text>
-    </View>
+      <NativeBaseProvider>
+        <View>
+            <Button
+                onPress={() => {
+                    window.alert("Presionado");
+                }
+                }
+            >
+                Continue
+            </Button>
+          <Text>Not implemented yet</Text>
+        </View>
+      </NativeBaseProvider>
   )
 }
 export default SubscriptionScreen
