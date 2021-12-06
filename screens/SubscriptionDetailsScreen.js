52b6aab04a34614aefbde29065875cc97d0eda49
DamianGanopolsky <damian98dgg@gmail.com>
2021-12-05 22:07:27 -0300

    collapse if button is clicked

--- screens/SubscriptionDetailsScreen.js
+++ screens/SubscriptionDetailsScreen.js
@@ -1,11 +1,14 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Text, View } from 'react-native'
 import CourseInSubscriptionCard from '../components/CourseInSubscriptionCard'
+import ConfirmationAlert from '../components/ConfirmationAlert'
 import {
   Box,
   VStack,
   NativeBaseProvider,
-  Button, Heading
+  Button, Heading,
+  ScrollView,
+  Collapse
 } from 'native-base'
 import session from '../session/token'
 
@@ -18,7 +21,7 @@ export default function StudentCourseDetailsScreen ({ navigation, route }) {
   const [courses, setCourses] = React.useState([])
   const tokenHeader = (session.firebaseSession) ? 'firebase_authentication' : 'facebook_authentication'
   const sessionToken = (session.firebaseSession) ? session.token : session.facebookToken
-
+  const [confirmation, setConfirmation] = React.useState(false)
   const getSuscriptionsCourses = () => {
     return fetch(suscriptionCoursesURL + subscription.key,
       { headers: { [tokenHeader]: sessionToken } })
@@ -53,38 +56,60 @@ export default function StudentCourseDetailsScreen ({ navigation, route }) {
     getSuscriptionsCourses()
   }, [])
 
+  function renderConfirmation () {
+    return (
+            <View>
+                <Text numberOfLines={1}></Text>
+                <Text>Detalles</Text>
+            </View>
+    )
+  }
+
+  function renderVoid () {
+
+  }
+
   return (
         <NativeBaseProvider>
-            <Box safeArea flex={0} p="2" w="90%" mx="auto" py="8">
-                <VStack alignItems="center">
-                    <Heading fontSize="xl">{subscription.suscriptionName}</Heading>
-                    <Heading fontSize="lg">Subscription price: {subscription.price}</Heading>
-                    <Text numberOfLines={1}></Text>
-                    <Heading fontSize="lg">Courses included in this subscription:</Heading>
-                    <Text numberOfLines={1}></Text>
-                    <Text numberOfLines={1}></Text>
-                    { courses.map(item => {
-                      return (
-                                <CourseInSubscriptionCard
-                                    key={item.key}
-                                    title={item.courseName}
-                                    price={item.price}
-                                    duration={item.duration}/>
-                      )
-                    }) }
-                    <Text numberOfLines={1}></Text>
-                    <Text numberOfLines={1}></Text>
-                    <Button
-                        onPress={() => {
-                          window.alert('Te suscribiste!. ')
-                          // Aca hago un post, y obtengo si se pudo suscribir o no
-                        }
-                        }
-                    >
-                        Subscribe
-                    </Button>
-                </VStack>
-            </Box>
+            <ScrollView>
+                <Collapse isOpen={confirmation}>
+                    {confirmation ? renderConfirmation() : renderVoid()}
+                </Collapse>
+                <Collapse isOpen={!confirmation}>
+                    <Box safeArea flex={0} p="2" w="90%" mx="auto" py="8">
+                        <VStack alignItems="center">
+                            <Heading fontSize="xl">{subscription.suscriptionName}</Heading>
+                            <Heading fontSize="lg">Subscription price: {subscription.price}</Heading>
+                            <Text numberOfLines={1}></Text>
+                            <Heading fontSize="lg">Courses included in this subscription:</Heading>
+                            <Text numberOfLines={1}></Text>
+                            <Text numberOfLines={1}></Text>
+
+                            { courses.map(item => {
+                              return (
+                                    <CourseInSubscriptionCard
+                                        key={item.key}
+                                        title={item.courseName}
+                                        price={item.price}
+                                        duration={item.duration}/>
+                              )
+                            }) }
+                            <Text numberOfLines={1}></Text>
+                            <Text numberOfLines={1}></Text>
+                            <Button
+                                onPress={() => {
+                                  setConfirmation(true)
+                                  // Aca hago un post, y obtengo si se pudo suscribir o no
+                                }
+                                }
+                            >
+                                Subscribe
+                            </Button>
+                        </VStack>
+                    </Box>
+                </Collapse>
+
+                </ScrollView>
         </NativeBaseProvider>
   )
 }
