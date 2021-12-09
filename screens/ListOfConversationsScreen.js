199ae0e75e26da9c617b7e1a1b564bfd51c0442e
DamianGanopolsky <damian98dgg@gmail.com>
2021-12-09 17:14:50 -0300

    get all users

--- screens/ListOfConversationsScreen.js
+++ screens/ListOfConversationsScreen.js
@@ -4,8 +4,25 @@ import React, {useState, useEffect, useCallback} from 'react'
 import {StyleSheet, Text, TextInput, View, YellowBox, Button} from 'react-native'
 import session from '../session/token'
 
-
+const apiGatewayBaseUrl = 'https://ubademy-api-gateway.herokuapp.com/api-gateway/'
+const getUsersUrl = 'https://ubademy-user-service.herokuapp.com/api/users'
 const ListOfConversationsScreen = ({ navigation }) => {
+    const tokenHeader = (session.firebaseSession) ? 'firebase_authentication' : 'facebook_authentication'
+    const sessionToken = (session.firebaseSession) ? session.token : session.facebookToken
+    console.log('session token:', sessionToken)
+    console.log('Header:', tokenHeader)
+
+
+    fetch(getUsersUrl,
+        {headers: {[tokenHeader]: sessionToken}})
+        .then((response) => response.json())
+        .then((json) => {
+            console.log("Users:",json)
+        })
+        .catch((error) => {
+            console.error(error)
+        })
+
 
     return (
         <View>
